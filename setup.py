019aae00c1ab3d8fbb06e8d707cda25f4e29870c
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-08-17 18:44:25 +0900

    setup.py 만들기

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="ycp",
+    version="0.0.1",
+    description='read descript thag writed by korean what write in github.',
+    packages=find_packages(exclude=[]),
+    install_requires=["subpr"],
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    keywords=["youtube", "clip", "playlist", "ycp"], url='https://github.com/FarAway6834/ycp'
+)
